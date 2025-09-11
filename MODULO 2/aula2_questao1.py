905872b87ccf43174e402ea7d9578dfde5da29a6
yasminprocopio32-cell <yasmin.procopio32@gmail.com>
2025-09-10 21:27:40 -0300

    Add files via upload

---
+++ MODULO 2/aula2_questao1.py
@@ -0,0 +1,7 @@
+n1 = 5
+n2 = 2
+print (type(n1))
+print (type (n2))
+n3 = n1 / n2
+print (n3)
+print (type (n3))
---
+++ MODULO 2/aula2_questao2.py
@@ -0,0 +1,9 @@
+frase = "o resultado é"
+n1 = 10
+n2 = 3.5
+resultado = n1 + n2
+print (frase, type (frase))
+print (n1, type (n1))
+print (n2, type (n2))
+print (resultado, type (resultado))
+
