9ac64b5e1ebc2786d65532b1ce9cf6215b7fed3a
yasminprocopio32-cell <yasmin.procopio32@gmail.com>
2026-02-24 16:27:01 -0300

    Delete MODULO 6/aula1_questao2.py

--- MODULO 6/aula1_questao2.py
+++
@@ -1,8 +0,0 @@
-import random
-
-num_elementos = random.randint(5, 20)
-elementos = [random.randint(1, 10) for _ in range(num_elementos)]
-
-print(elementos)
-print(sum(elementos))
-print(sum(elementos) / len(elementos))
